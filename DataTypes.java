dcc92a6e7c1913760642c08362efe2d38aee872e
joshua <notifjoshuarizzbon@gmail.com>
2025-12-03 13:30:14 +0000

    Commit

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypeExamples {
+
+    int myInt = 42;
+    double myDouble = 100.24;
+    char myChar = 'T';
+    boolean myBool = false;
+}
+
+public class DataTypes {
+    public static void main(String[] args) {
+        DataTypeExamples dt = new DataTypeExamples();
+        System.out.println("myInt: " + dt.myInt);
+
+
+    }
+}
