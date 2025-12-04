d795f94a2e0d45c613a1c74d33896f8836bf0a06
joshua <notifjoshuarizzbon@gmail.com>
2025-12-04 14:19:39 +0000

    No

---
+++ Variables.java
@@ -0,0 +1,33 @@
+class Student {
+
+    String studentName;
+    static int studentCount = 0;
+    static final String SCHOOL_NAME = "CSAEA";
+
+    Student(String n) {
+        studentName = n; 
+        System.out.println(n + " is a student at " + SCHOOL_NAME );
+
+        studentCount++;
+
+        System.out.println("Number of students: " + studentCount);
+    }
+
+}
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+        new Student("Alex");
+        new Student("Ben");
+        new Student("Benny");
+        new Student("Goober");
+        new Student("Sweetooth");
+        new Student("Bobo");
+
+        
+
+    }
+
+}
